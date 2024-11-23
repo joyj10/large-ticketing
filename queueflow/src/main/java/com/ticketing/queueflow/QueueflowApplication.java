81df9854441e514ae5ce168dc0aecf92ec841333
joyj10 <micool1030@gmail.com>
2024-11-23 23:07:24 +0900

    ticketing project start project

---
+++ queueflow/src/main/java/com/ticketing/queueflow/QueueflowApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.queueflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueflowApplication.class, args);
+	}
+
+}
---
+++ queueflow/src/test/java/com/ticketing/queueflow/QueueflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketing.queueflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
